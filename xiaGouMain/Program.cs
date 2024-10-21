b77ebf92242b2c28c41f7bf48f548f6ea171a152
AG130 <q2282536122@163.com>
2024-10-21 18:03:22 +0800

    jwt服务注册

--- xiaGouMain/Program.cs
+++ xiaGouMain/Program.cs
@@ -1,3 +1,5 @@
+using SupportLayer.JwtUtils;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region jwt服务注册
+//获取配置
+builder.Services.Configure<JwtTokenOptions>(builder.Configuration.GetSection("JwtTokenOptions"));
+//服务-接口绑定
+builder.Services.AddTransient<ICustomJwtService, CustomJwtService>();
+JwtTokenOptions tokenOptions = new JwtTokenOptions
+{
+    Issuer = "issuer",
+    Audience = "audience",
+    SecurityKey = "key"
+};
+//绑定jwt token可选项
+builder.Configuration.Bind("JwtTokenOptions", tokenOptions);
+
+#endregion
 
 var app = builder.Build();
 
